9ae86b4d5b4e6b4c63154532b055a72810d33ce8
Alexey Rychagov <kavabynga.ks@gmail.com>
2023-09-27 13:06:59 +0300

    message
    Signed-off-by: Alexey Rychagov <kavabynga.ks@gmail.com>

--- frontend/src/components/layouts/default-layout/components/main-header/MainHeader.vue
+++ frontend/src/components/layouts/default-layout/components/main-header/MainHeader.vue
@@ -1,5 +1,16 @@
 <template>
   <v-container>
-    <div style="flex-basis: 100%"></div>
+    <div style="flex-basis: 100%">
+      <v-row no-gutters>
+        <v-col
+          v-for="n in 3"
+          :key="n"
+          cols="12"
+          sm="4"
+        >
+          <v-sheet class="ma-2 pa-2">One of three columns</v-sheet>
+        </v-col>
+      </v-row>
+    </div>
   </v-container>
 </template>
